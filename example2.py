20dd1cbc695cf47faad233b805e4614525e5c3aa
Junbo Wang <juniwang@microsoft.com>
2017-10-16 13:24:33 +0800

    session and variable example

--- example2.py
+++ example2.py
@@ -1,3 +1,7 @@
+# disable warnings which are just informing you if you build TensorFlow from source it can be faster on your machine.
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+
 import tensorflow as tf
 import numpy as np
 
---
+++ session_example.py
@@ -0,0 +1,20 @@
+# disable warnings which are just informing you if you build TensorFlow from source it can be faster on your machine.
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+
+import tensorflow as tf
+
+matrix1 = tf.constant([[3, 3]])
+matrix2 = tf.constant([[2], [2]])
+product = tf.matmul(matrix1, matrix2) # matrix multipy np.dot(m1,m2)
+
+# mothod 1
+# sess = tf.Session()
+# result = sess.run(product)
+# print(result)
+# sess.close()
+
+# method 2
+with tf.Session() as sess:
+	result2 = sess.run(product)
+	print(result2)
