bf0f0d81043c3da5d871b3c875113bf242b14e9a
Junbo Wang <juniwang@microsoft.com>
2017-10-20 11:01:01 +0800

    show histogram and scalar gram

--- layer_example.py
+++ layer_example.py
@@ -6,12 +6,17 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
-def add_layer(inputs, in_size, out_size, activation_function=None):
+def add_layer(inputs, in_size, out_size,n_layer, activation_function=None):
+	layer_name = "layer%s" % n_layer
 	with tf.name_scope("layer"):
 		with tf.name_scope("weights"):
 			Weights = tf.Variable(tf.random_normal([in_size, out_size]), name='W')
+			# show histogram in tensorboard
+			# tf.histogram_summary(layer_name+"/weights", Weights) # tensorflow < 0.12
+			tf.summary.histogram(layer_name+"/weights", Weights) # tensorflow >= 0.12
 		with tf.name_scope("biases"):
 			biases = tf.Variable(tf.zeros([1, out_size]) + 0.1, name="b")
+			tf.summary.histogram(layer_name+"/baises", biases)
 		with tf.name_scope('Wx_plus_b'):
 			Wx_plus_b = tf.matmul(inputs, Weights) + biases
 		if activation_function is None:
@@ -19,6 +24,7 @@ def add_layer(inputs, in_size, out_size, activation_function=None):
 		else:
 			outputs = activation_function(Wx_plus_b)
 
+		tf.summary.histogram(layer_name+"/outputs", outputs)
 		return outputs
 
 
@@ -31,13 +37,15 @@ with tf.name_scope('inputs'):
 	ys = tf.placeholder(tf.float32, [None, 1], name='y_input')
 
 # add hidden layer
-l1 = add_layer(xs, 1, 10, activation_function=tf.nn.relu)
+l1 = add_layer(xs, 1, 10, n_layer=1, activation_function=tf.nn.relu)
 # add output layer
-prediction = add_layer(l1, 10, 1, activation_function=None)
+prediction = add_layer(l1, 10, 1, n_layer=2, activation_function=None)
 
 # error between prediction and real data
 with tf.name_scope('loss'):
 	loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction), reduction_indices=[1]))
+	# tf.scalar_summary("loss", loss) # tensorflow < 0.12
+	tf.summary.scalar("loss", loss) # tensorflow >= 0.12
 
 with tf.name_scope('train'):
 	train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
@@ -51,6 +59,9 @@ ax.scatter(x_data, y_data)
 plt.ion() # otherwise show() will block. 
 plt.show()
 
+
+# merged = tf.merge_all_summaries() # tensorflow < 0.12
+merged = tf.summary.merge_all() # tensorflow >= 0.12
 with tf.Session() as sess:
 	# visible output method 2, which can viewed by run: tensorboard  --logdir='logs/' 
 	writer =tf.summary.FileWriter('logs/', sess.graph)
@@ -65,6 +76,11 @@ with tf.Session() as sess:
 				ax.lines.remove(lines[0])
 			except Exception:
 				pass
+			# plot figures
 			prediction_value = sess.run(prediction, feed_dict={xs: x_data, ys: y_data})
 			lines = ax.plot(x_data, prediction_value, "r-", lw=5)
 			plt.pause(0.2)
+
+			# record histogram
+			rs = sess.run(merged, feed_dict={xs: x_data, ys: y_data})
+			writer.add_summary(rs, i)
